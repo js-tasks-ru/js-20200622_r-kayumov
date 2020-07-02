36e03b601c96201d252da9634c3fe70138b55ab4
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-02 16:49:21 +0300

    Added solution for task: trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,14 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  const firstSlice = string.slice(0, size);
+  const rest = [...string.slice(size)];
 
+  return rest.reduce((accum, item) => {
+    if (!accum.endsWith(item.repeat(size))) {
+      accum += item;
+    }
+
+    return accum;
+  }, firstSlice);
 }
